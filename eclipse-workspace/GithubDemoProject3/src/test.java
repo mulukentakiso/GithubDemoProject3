3065af801294147636e4a6ce770ca85f21f2b3e1
mulu takiso <me@mulut.com>
2020-08-19 14:45:25 -0400

    first commit...

---
+++ eclipse-workspace/GithubDemoProject3/src/test.java
@@ -0,0 +1,16 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		
+     String thirsty = "yes";
+     
+     if (thirsty == "yes")
+        System.out.println("drink something....");
+        else {
+    	 System.out.println("Do going .....");
+    	  
+      }
+	}
+
+}
